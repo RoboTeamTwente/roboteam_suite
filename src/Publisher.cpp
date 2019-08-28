c263c7acd5244268ae1a00762b6bae2793af8252
mrlukasbos <mrlukasbos@gmail.com>
2019-08-29 00:27:13 +0200

    add some documentation

--- src/Publisher.cpp
+++ src/Publisher.cpp
@@ -5,16 +5,17 @@
 #include "Publisher.h"
 #include <string>
 
+// create a publisher which transmits messages over a given topic
 roboteam_proto::Publisher::Publisher(std::string const& topic) {
   socket = new zmqpp::socket(context, zmqpp::socket_type::pub);
   socket->bind(PUBLISH_ENDPOINT);
   this->topic = topic;
 }
 
+// send a string over the topic
 void roboteam_proto::Publisher::send(std::string message) {
   std::string data = topic + message;
   zmqpp::message transmission;
   transmission << data;
   socket->send(transmission);
-  // std::cout << "sending data: " << message << " to topic: " << topic << std::endl;
 }
--- src/Subscriber.cpp
+++ src/Subscriber.cpp
@@ -5,7 +5,8 @@
 #include "Subscriber.h"
 #include <iostream>
 
-
+// create a subscriber with a callback function that gets called when new data is available
+// the new data will be available in the function.
 roboteam_proto::Subscriber::Subscriber(std::string topic, subscriberCallback func) {
   socket = new zmqpp::socket(context, zmqpp::socket_type::sub);
   socket->subscribe(topic);
@@ -15,6 +16,7 @@ roboteam_proto::Subscriber::Subscriber(std::string topic, subscriberCallback fun
   t1 = std::thread(&Subscriber::poll, this, &reactor);
 }
 
+// set up the callback with the response
 void roboteam_proto::Subscriber::setCallBack(subscriberCallback func) {
   zmqpp::poller * poller = &reactor.get_poller();
 
@@ -22,17 +24,19 @@ void roboteam_proto::Subscriber::setCallBack(subscriberCallback func) {
     zmqpp::message response;
     if(poller->has_input(* socket) ){
       socket->receive(response);
-      func(response.get(0));
+      func(response.get(0)); // call the subscriberCallback function
     }
   };
 
   reactor.add(* socket, callback);
 }
 
+// keep polling for new messages
 void roboteam_proto::Subscriber::poll(zmqpp::reactor * reactor) {
   while (reactor->poll()) { }
 }
 
+// when finished this function should be called
 void roboteam_proto::Subscriber::destroy() {
   t1.join();
 }
