b4532e95f787cfc68c3fdbe253e180a049042006
mrlukasbos <mrlukasbos@gmail.com>
2019-08-29 19:34:31 +0200

    apparently working version

--- include/Subscriber.h
+++ include/Subscriber.h
@@ -20,12 +20,25 @@ class Subscriber {
   zmqpp::socket * socket;
   zmqpp::reactor reactor;
   std::thread t1;
-  void createCallbackFunction(void (*subscriberCallback)(std::string));
   void init(const std::string &topic);
 
  public:
   template <class T>
-  Subscriber(std::string topic, void(T::* subscriberCallbackMethod)(std::string response), T& instance);
+  Subscriber(std::string topic, void(T::* subscriberCallbackMethod)(std::string response), T * instance){
+      init(topic);
+
+      zmqpp::poller * poller = &reactor.get_poller();
+      auto callback = [=](){
+        zmqpp::message response;
+        if(poller->has_input(* socket) ){
+          socket->receive(response);
+          (instance->*subscriberCallbackMethod)(response.get(0)); // call the subscriberCallback function
+        }
+      };
+      reactor.add(* socket, callback);
+
+      t1 = std::thread(&Subscriber::poll, this, &reactor);
+  }
 
   Subscriber(std::string topic, void(* subscriberCallbackMethod)(std::string response));
 
