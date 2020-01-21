f9efd7210e1175ca92299117060ccbd1d1a8c38d
Yuhanun Citgez <y.citgez@student.utwente.nl>
2020-01-21 15:34:31 +0100

    Added the files for all repos

---
+++ docker/tests/roboteam_world.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Cd into the suite
+cd /opt/roboteam/roboteam_suite
+# Make sure every branch is on master
+git submodule foreach git checkout master
+# Pull each
+git submodule foreach git pull
+# Cd into correct repo
+cd roboteam_world
+# Checkout branch name
+git checkout "$1"
+# Cd back and create + cd into build
+cd ..
+mkdir build
+cd build
+# Cmake generate build files
+cmake -DCMAKE_BUILD_TYPE=Debug -G "CodeBlocks - Ninja" ..
+# Cmake build
+cmake --build . --target world_tests -- -j $(nproc)
+echo "Finished compiling"
+# Run tests, xvfb-run simulates an xserver
+cd roboteam_world
+xvfb-run ./world_tests
