d60a482f4c9b9c56e7ad46583aa192349f2c8b35
Yuhanun <y.citgez@student.utwente.nl>
2020-03-31 12:44:19 +0200

    Updated docker files and bash files

--- docker/tests/roboteam_ai.sh
+++ docker/tests/roboteam_ai.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 
-# Cd into the suite
-cd /opt/roboteam/roboteam_suite
-# Pull each
-git submodule foreach git pull
-# Cd into correct repo
-cd roboteam_ai
-# Checkout branch name
-git checkout "$1"
-# Cd back and create + cd into build
-cd ..
-mkdir build
-cd build
-# Cmake generate build files
-cmake -DCMAKE_BUILD_TYPE=Debug -G "CodeBlocks - Ninja" .. -DCMAKE_CXX_COMPILER=/usr/lib/ccache/clang++ -DCMAKE_CC_COMPILER=/usr/lib/ccache/clang
 # Cmake build
 cmake --build . --target ai_tests -- -j $(nproc)
-echo "Finished compiling"
 # Run tests, xvfb-run simulates an xserver
 cd roboteam_ai
 xvfb-run ./ai_tests
--- docker/tests/roboteam_utils.sh
+++ docker/tests/roboteam_utils.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 
-# Cd into the suite
-cd /opt/roboteam/roboteam_suite
-# Pull each
-git submodule foreach git pull
-# Cd into correct repo
-cd roboteam_utils
-# Checkout branch name
-git checkout "$1"
-# Cd back and create + cd into build
-cd ..
-mkdir build
-cd build
-# Cmake generate build files
-cmake -DCMAKE_BUILD_TYPE=Debug -G "CodeBlocks - Ninja" ..
 # Cmake build
 cmake --build . --target utils_tests -- -j $(nproc)
-echo "Finished compiling"
 # Run tests, xvfb-run simulates an xserver
 cd roboteam_utils
 xvfb-run ./utils_tests
