5893e2776c41f481c6424990e2b07779bce6417e
Yuhanun Citgez <yuhanun.c@gmail.com>
2021-05-24 20:22:49 +0200

    Finished dockerfile

--- robocup/run.sh
+++ robocup/run.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-bashack
+export BASE_DIR=/opt/roboteam/roboteam_suite/build
+cd $BASE_DIR
+
+# observer
+cd ./roboteam_world
+./roboteam_world &
+
+# robothub
+cd ../roboteam_robothub
+./roboteam_robothub &
+
+# AI
+cd ../roboteam_ai
+xvfb-run ./roboteam_ai
