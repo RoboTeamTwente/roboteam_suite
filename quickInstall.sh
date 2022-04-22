9dee2755477f8c930af5a4de46219923dfc6ce88
Floris Heinen <Floris.heinen1@gmail.com>
2022-04-22 17:53:07 +0200

    Add install script

---
+++ quickInstall.sh
@@ -0,0 +1,12 @@
+git clone https://github.com/RoboTeamTwente/roboteam_ai.git
+git clone https://github.com/RoboTeamTwente/roboteam_robothub.git
+git clone https://github.com/RoboTeamTwente/roboteam_world.git
+git clone https://github.com/RoboTeamTwente/roboteam_utils.git
+git clone https://github.com/RoboTeamTwente/roboteam_networking.git
+git clone https://github.com/RoboTeamTwente/roboteam_interface.git
+
+cd roboteam_robothub
+
+git clone https://github.com/RoboTeamTwente/roboteam_embedded_messages.git
+
+cd ../
