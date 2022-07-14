e75720a3b6d32bb5884cc75252758fa024ac126d
martin <mamiksik@gmail.com>
2022-07-14 12:46:14 +0700

    Default to ai 0

--- autoRestart.sh
+++ autoRestart.sh
@@ -2,7 +2,7 @@
 
 while (true) do
    echo starting...
-   ./cmake-build-debug/roboteam_ai/roboteam_ai 1
+   ./cmake-build-debug/roboteam_ai/roboteam_ai 0
    exitcode=$?
    echo "exit code of command is $exitcode"
 done
