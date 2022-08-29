740d4a186172344add327dc0856fb1dba76a12da
Floris Heinen <Floris.heinen1@gmail.com>
2022-08-29 03:22:10 +0200

    TEST

--- docker/clone.sh
+++ docker/clone.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-git clone https://github.com/RoboTeamTwente/roboteam_suite /home/default/roboteam_suite
-git clone https://github.com/RoboTeamTwente/roboteam_ai /home/default/roboteam_suite/roboteam_ai
-git clone https://github.com/RoboTeamTwente/roboteam_world /home/default/roboteam_suite/roboteam_world
-git clone https://github.com/RoboTeamTwente/roboteam_robothub /home/default/roboteam_suite/roboteam_robothub
-git clone https://github.com/RoboTeamTwente/roboteam_networking /home/default/roboteam_suite/roboteam_networking
-git clone https://github.com/RoboTeamTwente/roboteam_utils /home/default/roboteam_suite/roboteam_utils
-git clone https://github.com/RoboTeamTwente/roboteam_embedded_messages /home/default/roboteam_suite/roboteam_robothub/roboteam_embedded_messages
-git clone https://github.com/RoboTeamTwente/roboteam_interface /home/default/roboteam_suite/roboteam_interface
+mkdir rtt && git clone https://github.com/RoboTeamTwente/roboteam_suite /rtt/roboteam_suite && git clone https://github.com/RoboTeamTwente/roboteam_ai /rtt/roboteam_suite/roboteam_ai && git clone https://github.com/RoboTeamTwente/roboteam_world /rtt/roboteam_suite/roboteam_world && git clone https://github.com/RoboTeamTwente/roboteam_robothub /rtt/roboteam_suite/roboteam_robothub && git clone https://github.com/RoboTeamTwente/roboteam_networking /rtt/roboteam_suite/roboteam_networking && git clone https://github.com/RoboTeamTwente/roboteam_utils /rtt/roboteam_suite/roboteam_utils && git clone https://github.com/RoboTeamTwente/roboteam_embedded_messages /rtt/roboteam_suite/roboteam_robothub/roboteam_embedded_messages && git clone https://github.com/RoboTeamTwente/roboteam_interface /rtt/roboteam_suite/roboteam_interface
